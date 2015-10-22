147c61c9b015a8b25e7fe28750ef0a99764137de
Yutian Li <hotpxless@gmail.com>
2015-10-22 14:18:54 -0700

    [master] add utils

---
+++ canonicalization/utils/imagenet_helper.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Authors: Oliver Groth, Yutian Li
+
+"""Contains helper functions for working with ImageNet."""
+
+import cPickle
+import os.path
+from nltk.corpus import wordnet
+from . import wordnet_helper
+
+
+class ImageNetHelper(object):
+
+    def __init__(self, res_path):
+        self.res_path = res_path
+        self.wn_helper = wordnet_helper.WordNetHelper()
+        self.offset_synset_dict = None
+        self.imagenet_index_dict = None
+
+    def get_all_wordnet_ids(self):
+        """Gets a list of synsets in ImageNet represented by IDs.
+
+        Returns:
+            A list of IDs.
+        """
+        with open(os.path.join(self.res_path, 'imagenet',
+                               'imagenet_synsets.txt')) as f:
+            return f.readlines()
+
+    def get_offset_synset_dict(self):
+        """Gets all synsets visualized in Imagenet.
+
+        Creates a new dictionary on the fly if there is none.
+
+        Returns:
+            The dictionary in the format of `{offset: synset}`.
+        """
+        if self.offset_synset_dict is None:
+            dict_path = os.path.join(self.res_path, 'imagenet',
+                                     'offset_synset.dict')
+            if os.path.isfile(dict_path):
+                with open(dict_path, 'rb') as f:
+                    self.offset_synset_dict = cPickle.load(f)
+            else:
+                with open(dict_path, 'wb') as f:
+                    syns = list(wordnet.all_synsets())
+                    offsets_list = [(s.offset(), str(s)) for s in syns]
+                    self.offset_synset_dict = dict(offsets_list)
+                    cPickle.dump(self.offset_synset_dict, f,
+                                 protocol=cPickle.HIGHEST_PROTOCOL)
+        return self.offset_synset_dict
+
+    def wordnet_id_to_synset(self, wordnet_id):
+        """Gets the synset associated with a WordNet ID.
+
+        Args:
+            wordnet_id: A WordNet ID.
+
+        Returns:
+            The synset.
+
+        Raises:
+            KeyError: Cannot find the corresponding WordNetID.
+        """
+        offset = int(wordnet_id[1:].lstrip('0'))
+        return self.get_offset_synset_dict()[offset]
+
+    def get_imagenet_index(self):
+        """Gets a mapping of all synsets in ImageNet to their WordNet IDs.
+
+        Returns:
+            The dictionary in the format of `{synset: wordnet_id}`.
+        """
+        if self.imagenet_index_dict is None:
+            dict_path = os.path.join(
+                self.res_path, 'imagenet', 'imagenet_index.dict')
+            if os.path.isfile(dict_path):
+                with open(dict_path, 'rb') as f:
+                    self.imagenet_index_dict = cPickle.load(f)
+            else:
+                with open(dict_path, 'wb') as f:
+                    index_list = []
+                    for i, wordnet_id in enumerate(self.get_all_wordnet_ids()):
+                        entry = (
+                            str(self.wordnet_id_to_synset(wordnet_id)),
+                            wordnet_id)
+                        index_list.append(entry)
+                    self.imagenet_index_dict = dict(index_list)
+                    cPickle.dump(self.imagenet_index_dict, f,
+                                 protocol=cPickle.HIGHEST_PROTOCOL)
+        return self.imagenet_index_dict
+
+    def in_imagenet(self, synset):
+        """Checks whether a synset is in ImageNet.
+
+        Args:
+            synset: The synset to check.
+
+        Returns:
+            Whether it is in ImageNet.
+        """
+        wordnet_id = self.wn_helper.synset_to_wordnet_id(synset)
+        return wordnet_id in self.get_imagenet_index()
---
+++ canonicalization/utils/wordnet_helper.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Authors: Oliver Groth, Yutian Li
+
+"""Contains various helper functions for working with WordNet."""
+
+from nltk.corpus import wordnet
+import collections
+
+
+class WordNetHelper(object):
+
+    def lemma_counter(self, word, pos=wordnet.NOUN):
+        """Gets frequency counts for a synset.
+
+        Args:
+            word: A word to lookup.
+            pos: Part of speech.
+
+        Returns:
+            A collections.Counter for all synsets of the
+            specified word with frequencies annotated.
+        """
+        syns = wordnet.synsets(word, pos)
+        freq = []
+        for s in syns:
+            for l in s.lemmas():
+                count = [s] * l.count()
+                # Extend frequency list by count times the synset ID.
+                freq.extend(count)
+        return collections.Counter(freq)
+
+    def synset_to_wordnet_id(self, syn):
+        """Converts synset to WordNet ID.
+
+        Args:
+            syn: A synset.
+
+        Returns:
+            A unique WordNet ID as used in ImageNet.
+        """
+        pos = str(syn.pos())
+        offset = str(syn.offset())
+        len_offset = len(offset)
+        # Pad offset number with leading zeros.
+        if len_offset < 8:
+            pad = 8 - len_offset
+            offset = '0' * pad + offset
+        return pos + offset

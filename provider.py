5d5b6f48ae529c17c7a7952d3c7abe09e96d052f
Yutian Li <hotpxless@gmail.com>
2015-10-23 11:37:31 -0700

    [master] provider

---
+++ provider.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Author: Yutian Li
+from __future__ import absolute_import
+from __future__ import print_function
+import requests
+
+
+class CanonicalizationProvider(object):
+
+    def __init__(self, server_addr):
+        self.server_addr = server_addr
+
+    def canonicalize(self, word):
+        payload = {'word': word}
+        r = requests.post(self.server_addr, json=payload)
+        if r.status_code != 200:
+            raise ValueError('unrecognized format')
+        result = r.json()
+        if 'result' in result:
+            return result['result']
+        else:
+            raise ValueError('unrecognized format')
+
+
+if __name__ == '__main__':
+    c = CanonicalizationProvider('http://54.88.99.94:20001/canonicalize')
+    print(c.canonicalize('dog'))

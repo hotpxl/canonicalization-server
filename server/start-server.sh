895474a268937665e787642249991776d45b1b99
Yutian Li <hotpxless@gmail.com>
2016-07-20 00:19:40 +0800

    [master] Change dir.

--- server/start-server.sh
+++ server/start-server.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 set -euo pipefail
+
+file_path="$(readlink -f "$(dirname "${BASH_SOURCE[0]}")")"
+pushd "${file_path}" > /dev/null
+
 gunicorn -c gunicorn_config.py server:app --access-logfile='-'

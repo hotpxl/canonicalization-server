55f738adf6f6ab9261653ada4e4bfbd8f32ad5de
Yutian Li <hotpxless@gmail.com>
2016-07-21 01:05:51 +0800

    [master] Add punkt.

--- get-nltk-data.sh
+++ get-nltk-data.sh
@@ -4,4 +4,4 @@ set -euo pipefail
 file_path="$(readlink -f "$(dirname "${BASH_SOURCE[0]}")")"
 pushd "${file_path}" > /dev/null
 
-python -m nltk.downloader -d nltk_data wordnet wordnet_ic stopwords
+python -m nltk.downloader -d nltk_data wordnet wordnet_ic stopwords punkt
